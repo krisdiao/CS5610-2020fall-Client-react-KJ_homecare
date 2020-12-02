3a4ba532c61dc323a394924ebd5cad7e417b2d2d
yxhcj <yxhcj19@gmail.com>
2020-12-02 00:51:01 -0500

    adding review page

--- src/components/AboutKJ/ReviewsViewComponent.js
+++ src/components/AboutKJ/ReviewsViewComponent.js
@@ -1,18 +1,17 @@
 import React from "react";
 import reviewService from "../../services/ReviewService";
-import {Link} from "react-router-dom";
 import Carousel from 'react-bootstrap/Carousel'
+import StarRatingComponent from "../User/LoginUsers/StarRatingComponent"
 
 
-
-export class ReviewsViewComponent extends React.Component{
+export class ReviewsViewComponent extends React.Component {
     // state ={
     //     reviews:[],
     // }
-    state ={
-        reviews:[
+    state = {
+        reviews: [
             {
-                id:"1",
+                id: "1",
                 title: "a",
                 firstName: "qqqqq",
                 lastName: "mmmmm",
@@ -20,7 +19,7 @@ export class ReviewsViewComponent extends React.Component{
                 stars: 2.5
             },
             {
-                id:"2",
+                id: "2",
                 title: "b",
                 firstName: "wwwwwww",
                 lastName: "nnnnnn",
@@ -28,7 +27,7 @@ export class ReviewsViewComponent extends React.Component{
                 stars: 1
             },
             {
-                id:"3",
+                id: "3",
                 title: "c",
                 firstName: "eeeee",
                 lastName: "zzzzz",
@@ -37,131 +36,46 @@ export class ReviewsViewComponent extends React.Component{
 
             },
         ],
+        space: " "
     }
 
     componentDidMount() {
         reviewService.findAllReviews()
-            .then(reviews =>{
-                this.setState( {
+            .then(reviews => {
+                this.setState({
                     reviews: reviews
                 })
             })
     }
+
+
     render() {
         return(
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
-                        alt="Third slide"
-                    />
+                <Carousel>
+                    {this.state.reviews.map(review =>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="/review-background1.jpg"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                                <i class="fa fa-quote-left fa-3x fa-pull-left fa-border" aria-hidden="true"></i>
+                                <h2>{review.title}</h2>
+                                <br/>
+                                <StarRatingComponent stars={review.stars} editing={false}/>
+                                <br/>
+                                <h5>{review.content}</h5>
+                                <br/>
+                                {review.firstName}{this.state.space}{review.lastName}
+                                {this.state.space}
+                                {this.state.space}
+                                {review.timeStamp.toString().substr(0, 10)}
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )}
+                </Carousel>
         )
     }
 }
-// export class ReviewsViewComponent extends React.Component{
-//     // state ={
-//     //     reviews:[],
-//     // }
-//     state ={
-//         reviews:[
-//             {
-//                 id:"1",
-//                 title: "a",
-//                 firstName: "qqqqq",
-//                 lastName: "mmmmm",
-//                 timeStamp: new Date(),
-//                 stars: 2.5
-//             },
-//             {
-//                 id:"2",
-//                 title: "b",
-//                 firstName: "wwwwwww",
-//                 lastName: "nnnnnn",
-//                 timeStamp: new Date(),
-//                 stars: 1
-//             },
-//             {
-//                 id:"3",
-//                 title: "c",
-//                 firstName: "eeeee",
-//                 lastName: "zzzzz",
-//                 timeStamp: new Date(),
-//                 stars: 5
-//
-//             },
-//         ],
-//     }
-//
-//     componentDidMount() {
-//         reviewService.findAllReviews()
-//             .then(reviews =>{
-//                 this.setState( {
-//                     reviews: reviews
-//                 })
-//             })
-//     }
-//     render() {
-//         return(
-//             <div className="container">
-//                 <table className="table table-hover ">
-//                     <thead>
-//                     <tr>
-//                         <th>Title</th>
-//                         <th>Last Name</th>
-//                         <th>First Name</th>
-//                         <th>Last Modified</th>
-//                     </tr>
-//                     </thead>
-//                     <tbody>
-//                     {this.state.reviews.map(review =>
-//                         <tr>
-//                             <td>
-//                                 <Link to={{
-//                                     pathname: `/update-review/${review.id}`,
-//                                     reviewViewProps: { review: review }
-//                                 }}
-//                                 > {review.title}</Link>
-//                             </td>
-//                             <td>{review.lastName}</td>
-//                             <td>{review.firstName}</td>
-//                             <td>{review.timeStamp.toString()}</td>
-//                             <td>{review.stars}</td>
-//                         </tr>
-//                     )}
-//                     </tbody>
-//                 </table>
-//             </div>
-//         )
-//     }
-// }
