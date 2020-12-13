0ffbfa3d7f8bc8b84cc652fd5adb872a3d952782
krisdiao <527369570@qq.com>
2020-12-13 02:24:36 -0500

    update privacy and term of use

--- src/components/header-footer/PrivacyPolicy.js
+++ src/components/header-footer/PrivacyPolicy.js
@@ -19,6 +19,90 @@ export const PrivacyPolicy = () => {
                 you are using, operating system, what brought you to our Website, as well as which of our Web pages
                 you have accessed.
             </p>
+            <p>Additionally, if you communicate with us regarding our Website or our services, we will collect any
+                information that you provide to us in any such communication.
+            </p>
+            <p>We may contact you via email in the future to tell you about specials, new products or services, or
+                changes to this privacy policy.
+            </p>
+            <h4>Information Use</h4>
+            <p>We use the collected information primarily for our own internal purposes, such as providing, maintaining,
+                evaluating, and improving our services and Website, fulfilling requests for information, and providing
+                customer support.
+            </p>
+            <h4>Security</h4>
+            <p>We follow generally accepted industry standards to protect the information submitted to us,
+                both during transmission and once we receive it.
+            </p>
+            <p>If we collect sensitive information (such as credit card data), that information is encrypted and
+                transmitted to us in a secure way. You can verify this by looking for a closed lock icon at the bottom
+                of your web browser, or looking for "https" at the beginning of the address of the web page.
+
+            </p>
+            <p>While we use encryption to protect sensitive information transmitted online, we also protect your information
+                offline. Only employees who need the information to perform a specific job (for example, billing or customer
+                service) are granted access to personally identifiable information. The computers/servers in which we store
+                personally identifiable information are kept in a secure environment.
+
+            </p>
+            <h4>Cookies</h4>
+            <p>We use "cookies" on this site. A cookie is a piece of data stored on a site visitor's hard drive to help us
+                improve your access to our site and identify repeat visitors to our site. For instance, when we use a cookie
+                to identify you, you would not have to log in a password more than once, thereby saving time while on our site.
+                Cookies can also enable us to track and target the interests of our users to enhance the experience on our site.
+                Usage of a cookie is in no way linked to any personally identifiable information on our site.
+            </p>
+            <h4>Sharing</h4>
+            <p>We will not sell or otherwise provide the information we collect to outside third parties for the purpose
+                of direct or indirect mass email marketing.
+
+            </p>
+            <p>We will disclose personal information and/or an IP address, when required by law or in the good-faith belief
+                that such action is necessary to:
+            </p>
+            <li>Cooperate with the investigations of purported unlawful activities and conform to the edicts of the law or
+                comply with legal process served on our company
+            </li>
+            <li>Protect and defend the rights or property of our Website and related properties
+            </li>
+            <li>Identify persons who may be violating the law, the rights of third parties, or otherwise misusing our
+                Website or its related properties
+            </li>
+            <p>Please keep in mind that whenever you voluntarily disclose personal information online - for example through e-mail,
+                discussion boards, or elsewhere - that information can be collected and used by others. In short, if you
+                post personal information online that is accessible to the public, you may receive unsolicited messages
+                from other parties in return.
+
+            </p>
+            <p>Ultimately, you are solely responsible for maintaining the secrecy of your personal information. Please be
+                careful and responsible whenever you are online.
+            </p>
+            <h4>Links</h4>
+            <p>This Website may contain links to other sites. Please be aware that we are not responsible for the content
+                or privacy practices of such other sites. We encourage our users to be aware when they leave our site and
+                to read the privacy statements of any other site that collects personally identifiable information.
+
+            </p>
+            <h4>Surveys & Contests</h4>
+            <p>From time-to-time our site may request information via surveys or contests. Participation in these surveys
+                or contests is completely voluntary and you may choose whether or not to participate and therefore disclose
+                this information. Information requested may include contact information (such as name and shipping address),
+                and demographic information (such as zip code, age). Contact information will be used to notify the winners
+                and award prizes. Survey information will be used for purposes of monitoring or improving the use and
+                satisfaction of this site.
+
+            </p>
+            <h4>Consent</h4>
+            <p>By using this Website, you consent to the collection and use of information as specified above. If we make
+                changes to our Privacy Policy, we will post those changes on this page. Please review this page frequently
+                to remain up-to-date with the information we collect, how we use it, and under what circumstances we disclose it.
+                You must review the new Privacy Policy carefully to make sure you understand our practices and procedures.
+
+            </p>
+            <p>If you feel that we are not abiding by this privacy policy, you should contact us immediately via telephone
+                at 336-457-1167 or via mail Attn: Rural Hall, NC, 27045.
+            </p>
+
         </div>
     );
 }
--- src/components/header-footer/TermsOfUse.js
+++ src/components/header-footer/TermsOfUse.js
@@ -5,6 +5,219 @@ export const TermsOfUse = () => {
         <div>
             <h1 className="orange">Terms of Use</h1>
             <br/>
+            <h4>Email Terms</h4>
+            <p>This message is intended only to the use of the individual or entity to which it is addressed, and may contain
+                    information that is privileged, confidential, and exempt from disclosing under applicable law. If you or
+                    your agent are not the intended recipient you are hereby notified that any dissemination, distribution,
+                    or copying of this communication is strictly prohibited. If you have received this communication in error,
+                    please notify us immediately by telephone or email and delete the communication from your inbox, deleted
+                    folder, and any instance of this communication on your email server.
+
+            </p>
+
+            <h4>CSA Terms</h4>
+            <p>Certified Senior Advisors (CSAs) have supplemented their individual professional licenses, credentials,
+                        and education with knowledge about aging and working with seniors. The CSA designation alone does
+                    not imply expertise in financial, health or social matters. For details, visit: www.csa.us.
+
+            </p>
+            <h4>NOTICE OF PRIVACY PRACTICES</h4>
+            <h5>THIS NOTICE DESCRIBES HOW MEDICAL INFORMATION ABOUT YOU MAY BE USED AND DISCLOSED AND HOW YOU CAN GET
+                    ACCESS TO THIS INFORMATION. PLEASE REVIEW IT CAREFULLY.
+            </h5>
+            <p>It is Griswold Home Care’s policy that all protected health information, including demographic and contact data,
+                    collected and maintained by the company only be used as necessary for treatment, payment, and healthcare
+                    operations purposes; to comply with applicable law; as otherwise indicated in this notice; and as
+                    authorized by you. Griswold Home Care will attempt to mitigate, to the extent practicable, any harmful
+                    effects from its misuse or inappropriate disclosure of your protected health information.
+            </p>
+            <p>Pursuant to applicable federal and state laws, all client records shall be maintained for the required number
+                    of years from the date of last service rendered. The Director of your servicing Griswold Home Care Office
+                    shall be responsible for maintaining custody and confidentiality of your records.
+
+            </p>
+            <h4>Uses and Disclosure of Protected Health Information</h4>
+            <p>Griswold Home Care may use and disclose your protected health information for treatment, payment,
+                    and healthcare operations purposes, as follows:</p>
+                <li>
+                        Treatment purposes: to appropriate parties to ensure you receive proper care.
+                        <li>For example, we may share your protected health information with your primary care provider
+                                or other treating physician(s), emergency transports, hospital emergency rooms, and referred
+                                caregivers.</li>
+                        <li>Payment purposes: to help referred caregivers receive payment for their services
+                        </li>
+                </li>
+
+                <li>For example, we may share information with your long-term care insurance provider or other third-parties
+                        responsible for paying your bills. If you pay out of pocket, in full, for care, you may request that
+                        protected health information related to that care be restricted from disclosure to health plans.
+                        <li>Healthcare operations purposes: to assess and to improve our referral services.
+                        </li>
+                        <li>For example, we may share information with an independent quality and client satisfaction
+                                surveyor who may contact you to assess the quality of our referral services.
+                        </li>
+                </li>
+                <li>When we share your protected health information with a third-party business associate, such as an independent
+                        quality and client satisfaction surveyor, we will have a written contract with the third party requiring
+                        it to protect the privacy of your disclosed protected health information.
+                </li>
+                <p>We may use or disclose protected health information, without your written authorization or the opportunity
+                        for you to agree or object, as follows:
+                </p>
+                <li>To the extent required by law, provided the use or disclosure complies with and is limited to the law’s relevant requirements;
+                </li>
+                <li>For public health activities, including: to a public health authority for preventing or controlling disease,
+                    injury, or disability; for purposes related to the quality, safety or effectiveness of an FDA-regulated
+                    product or activity; to a person who may have been exposed to a communicable disease or may otherwise be
+                    at risk of contracting or spreading a disease or condition; to an employer, about an individual who is a
+                    member of the workforce of the employer; and to a school, about an individual who is a student or prospective
+                    student of the school;
+                </li>
+                <li>About an individual whom we reasonably believe to be a victim of abuse, neglect, or domestic violence
+                    to a public health authority or government authority, including a social service or protective services agency;
+                </li>
+                <li>To a health oversight agency for oversight activities authorized by law or necessary for appropriate
+                    oversight, including audits, investigations, inspections, and other actions;
+                </li>
+                <li>In the course of any judicial or administrative proceeding, if expressly authorized by a court or
+                    administrative tribunal order or in response to a subpoena, discovery request, or other lawful process;
+                </li>
+                <li>For a law enforcement purpose to a law enforcement official: if pursuant to process or as otherwise
+                    required by law; to identify or locate a suspect, fugitive, material witness, or missing person;
+                    about an individual who is or is suspected to be a victim of a crime; to alert law enforcement of a death,
+                    if we suspect the death resulted from criminal conduct; if we believe it constitutes evidence of criminal
+                    conduct on our premises; and in response to a medical emergency, if necessary to alert law enforcement
+                    to the commission and nature of a crime, the location or victim(s) of such crime, and the identity,
+                    description, and location of the perpetrator;
+                </li>
+            <li>To a coroner or medical examiner for the purpose of identifying a deceased person, determining a cause of
+                death, or other duties;
+
+            </li>
+            <li>To organ procurement organizations or other entities engaged in the procurement, banking, or transplantation
+                of cadaveric organs, eyes, or tissue for the purpose of facilitating transplantation;
+
+            </li>
+            <li>For research, under certain conditions, regardless of the source of funding of the research;
+            </li>
+            <li>
+            To a person or persons reasonably able to prevent or lessen a serious and imminent threat to the health or
+            safety of a person or the public, including to the target of the threat, if we believe, in good faith, the
+            use or disclosure is necessary, or to law enforcement authorities to identify or apprehend an individual;
+            </li>
+            <li>For specialized government functions, including: if you are Armed Forces personnel, activities deemed
+                necessary by appropriate military command authorities to assure the proper execution of the military
+                mission; to federal officials for lawful intelligence, counter-intelligence, and other national security
+                activities, the provision of protective services to the President, foreign heads of state, or other
+                authorized persons, or for certain investigations; and about an inmate or individual to a correctional
+                institution or a law enforcement official having lawful custody of such inmate or other individual; and
+
+            </li>
+            <li>As authorized by, and to comply with, laws relating to workers’ compensation or other similar programs,
+                established by law, that provide benefits for work-related injuries or illness without regard to fault.
+
+            </li>
+            <p>We may use or disclose your protected health information for the following purposes, provided that we
+                inform you in advance of the use or disclosure and give you the opportunity to agree to or prohibit or
+                restrict the use or disclosure. We may orally inform you of and obtain your oral agreement or objection
+                to these uses and disclosures.
+
+            </p>
+            <li>Involvement with care or payment: we may disclose to a family member, other relative, close personal friend,
+                or any other person identified by you, the protected health information directly relevant to such person’s
+                involvement with your health care or payment related to your health care.
+
+            </li>
+            <li>Notification: we may use or disclose your protected health information to notify, or assist in the notification
+                of (including identifying or locating), a family member, your personal representative, or another person
+                responsible for your care, of your location, general condition, or death.
+
+            </li>
+            <li>Disaster Relief: we may use or disclose your protected health information to entities authorized by law
+                to assist in disaster relief efforts, for the purpose of coordinating notification as described above.
+
+            </li>
+            <p>If you are present for, or available prior to, the uses and disclosures in the immediately preceding paragraph,
+                we may only use or disclose your protected health information if: 1) we obtain your agreement, 2) we provide
+                you the opportunity to object and you do not object, or 3) we reasonably infer in our professional judgment
+                that you do not object. If you are not present, or if the opportunity to agree or object cannot be provided
+                because of your incapacity or an emergency, we will exercise our professional judgment to determine whether
+                the disclosure is in your best interests and will disclose only the information directly relevant to the
+                person’s involvement or needed for notification purposes. If you are deceased, we may disclose protected
+                health information relevant to a family member or other person who was involved in your care or payment
+                for health care prior to your death, unless doing so is inconsistent with your prior expressed preference
+                known to us.
+
+            </p>
+            <p>Any uses or disclosures of your protected health information not listed above, or otherwise required by law,
+                will be made only with your written authorization, which you may revoke in writing at any time. Upon receipt
+                of your written revocation of an authorization, Griswold Home Care will cease to use or disclose your protected
+                health information in the previously authorized manner. The written authorization requirement includes most
+                uses and disclosures of psychotherapy notes, most uses and disclosures of protected health information for
+                marketing purposes, and the sale of protected health information.
+            </p>
+
+            <h4>Client Privacy Rights
+            </h4>
+            <h5>Griswold Home Care is required to:
+            </h5>
+
+            <li>
+                Request that communications of protected health information be received by alternative means or at alternative locations;
+            </li>
+            <li>
+                Inspect and copy your protected health information;
+            </li>
+            <li>
+                Amend your protected health information;
+            </li>
+            <li>
+                Receive an accounting of the disclosures of your protected health information; and
+            </li>
+            <li>
+                Obtain a paper copy of this Notice of Privacy Practices.
+            </li>
+            <p>You also have the right to complain to Griswold Home Care and to the Secretary of the Department of
+                Health and Human Services if you believe your privacy rights have been violated.
+            </p>
+            <h4>Griswold Home Care’s Duties
+            </h4>
+            <p>You have the right to request restrictions on certain uses and disclosures of your protected health
+                information. Griswold Home Care is not required to agree to these requests.
+            </p>
+            <h5>You further have the right to:
+
+            </h5>
+            <li>Maintain the privacy of your protected health information;
+            </li>
+            <li>Provide you with notice of our legal duties and privacy practices with respect to your protected health information;
+            </li>
+            <li>Provide you with access to an electronic copy of your protected health information, in the electronic form
+                and format requested by you, if it is readily producible, or, if not readily producible, in an electronic
+                form and format as agreed to by us and you; if you decline to accept any of the electronic formats readily
+                producible by us, we must provide you a hard copy to fulfill your request; we may impose a reasonable
+                cost-based fee to review the access request and to produce the electronic copy;
+            </li>
+            <li>Transmit a copy of protected health information, if requested by you, directly to another person designated
+                by you; such a request must be in writing, be signed by you, and clearly identify the designated person
+                and where to send the copy of the protected health information;
+            </li>
+            <li>Notify you following a breach of your unsecured protected health information; and
+            </li>
+            <li>Abide by the terms of the Notice of Privacy Practices that is in effect.
+            </li>
+            <p>Griswold Home Care reserves the right to change the terms of this notice at any time and to immediately
+                make the new notice provisions effective for all protected health information that it maintains. If the
+                terms of this notice are materially changed, Griswold Home Care will promptly provide you with the revised
+                notice by mail.
+
+            </p>
+            <h4>Further Information
+            </h4>
+            <p>For further information about this notice, please contact the Quality and Compliance Department at
+                Home Care’s corporate headquarters at  336-457-1167 or in writing at  Rural Hall, NC, 27045.
+            </p>
+
         </div>
     );
 }
